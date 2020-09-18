89f52071f01d7a4d29083a20cb817cb064fd2981
phtn <phtn458@gmail.com>
2020-09-18 01:33:04 -0400

    updated content1

--- src/components/ContentOne.js
+++ src/components/ContentOne.js
@@ -13,41 +13,39 @@ const styles = {
     fontFamily: "Roboto, sans-serif",
     fontWeight: 300,
     fontSize: 28,
-    color: "#066b98"
+    color: "#066b98",
   },
   hr: {
     maxWidth: 100,
     borderBottom: "none",
-    height: '1px',
-    borderTopColor: "#ffd454"
+    height: "1px",
+    borderTopColor: "#ffd454",
   },
   description: {
     fontFamily: "Roboto, sans-serif",
-    fontSize: '1.3em',
-    width: '100%',
-    margin: '30px auto',
-  }
+    fontSize: "1.3em",
+    width: "100%",
+    margin: "30px auto",
+  },
 };
 
-const ContentOne = props => {
-  const { width } = props
-  const [padding, setPadding] = useState(100)
-  
-  
-  
-  useEffect(()=> {
-    const handlePadding = (pad) => setPadding(pad)  
+const ContentOne = (props) => {
+  const { width } = props;
+  const [padding, setPadding] = useState(100);
 
-    if (width < 767){
-      handlePadding(15)
-      // console.log('test')
-    } else {
-      handlePadding(100)
-    }
-    
-  }, [width])
+  useEffect(
+    () => {
+      const handlePadding = (pad) => setPadding(pad);
 
-  
+      if (width < 767) {
+        handlePadding(15);
+        // console.log('test')
+      } else {
+        handlePadding(100);
+      }
+    },
+    [width]
+  );
 
   return (
     <div style={styles.container}>
@@ -58,14 +56,23 @@ const ContentOne = props => {
               <Grid.Column>
                 <h2 style={styles.title}>{props.title}</h2>
                 <hr style={styles.hr} />
-                <p style={Object.assign({}, styles.description, {paddingRight: padding, paddingLeft: padding})}> <strong>React</strong> is the most advanced UI library used by thousands of companies because of its simplicity and component focused approach on applications putting reusability at top priority therefore making apps lightning fast.</p>
+                <p
+                  style={Object.assign({}, styles.description, {
+                    paddingRight: padding,
+                    paddingLeft: padding,
+                  })}
+                >
+                  {" "}
+                  <strong>Cartesian</strong> is a group of creative indiviuals
+                  that explore art and science towards human advancement.
+                </p>
               </Grid.Column>
               <Grid.Column>
-                  <Image
-                    src={props.image}
-                    height={300}
-                    style={{ margin: "0 auto" }}
-                  />
+                <Image
+                  src={props.image}
+                  height={300}
+                  style={{ margin: "0 auto" }}
+                />
               </Grid.Column>
             </Grid.Row>
           </Grid>
